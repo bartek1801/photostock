9b49985adaf99f15c81d49689cb1af06cc718c54
Bartlomiej <lechbartlomiej1801@gmail.com>
2017-08-15 21:28:08 +0200

    second commit

---
+++ src/pl/com/bottega/photostock/sales/model/Purchase.java
@@ -0,0 +1,9 @@
+package pl.com.bottega.photostock.sales.model;
+
+/**
+ * Created by bartek on 06.08.2017.
+ */
+public class Purchase {
+
+
+}
---
+++ src/pl/com/bottega/photostock/sales/ui/ConsoleAplication.java
@@ -0,0 +1,13 @@
+package pl.com.bottega.photostock.sales.ui;
+
+/**
+ * Created by bartek on 06.08.2017.
+ */
+public class ConsoleAplication {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Photostock");
+    }
+}
